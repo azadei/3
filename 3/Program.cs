01ddbdc188619bf9d0258c2c3f4b8b1eb66c8e2d
azadei <azadei@code.edu.az>
2023-07-17 14:47:44 +0400

    1

---
+++ 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        int[] numbers = { 9, 8, 6, 5 };
+
+        var query = from num in numbers
+
+                    select new { Number = num, sqrno = num * num};
+
+        foreach (var result in query)
+
+        {
+            Console.WriteLine("result number = " + result.Number + " " + "result query = " + result.sqrno );
+
+        }
+
+
+
+
+
+
+
+    }
+        
+    }
+
